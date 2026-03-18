ffeb65bbd7c453967bd780abf8a59dc6fb1ef2a1
Niedwin <G.Viure@outlook.fr>
2026-03-18 11:45:25 +0100

    Update whatever.hpp

--- CPP07/ex00/whatever.hpp
+++ CPP07/ex00/whatever.hpp
@@ -10,3 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iomanip>
+#include <string>
+
+template<typename T>
+T swap(T a, T b)
+{
+    T temp;
+    temp = b;
+    b = a;
+    a = temp;
+}
+
+T min(T a, T b)
+{
+    return (a < b ? a : b);
+}
+
+T min(T a, T b)
+{
+    return (a > b ? a : b);
+}
+#endif
